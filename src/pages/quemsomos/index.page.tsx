65705d052907516e1b2cfa3aec05dcca183b7c60
LeoGalisse <leonardogalisse@gmail.com>
2023-05-16 23:09:22 -0300

    feat: add frentes de atuacao section on quem somos page

--- src/pages/quemsomos/index.page.tsx
+++ src/pages/quemsomos/index.page.tsx
@@ -6,6 +6,7 @@ import {
 } from './styles'
 import minasGerais from '../../assets/minas-gerais.svg'
 import { AboutSection } from './components/AboutSection'
+import { FrentesDeAtuacaoSection } from './components/FrentesDeAtuacaoSection'
 
 export default function QuemSomos() {
   return (
@@ -21,6 +22,7 @@ export default function QuemSomos() {
         />
       </QuemSomosContent>
       <AboutSection />
+      <FrentesDeAtuacaoSection />
     </QuemSomosContainer>
   )
 }
