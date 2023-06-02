ec183c8665bde9a7387cc418c4fda55b17a13f33
LeoGalisse <leonardogalisse@gmail.com>
2023-06-01 21:56:12 -0300

    feat: add frentes de atuacao section update form

--- src/pages/update/index.page.tsx
+++ src/pages/update/index.page.tsx
@@ -1,12 +1,14 @@
 import { Title, UpdateContainer } from './styles'
 
 import { About } from './components/About'
+import { FrentesAtuacao } from './components/FrentesAtuacao'
 
 export default function Update() {
   return (
     <UpdateContainer>
       <Title>Alteração de conteúdo</Title>
       <About />
+      <FrentesAtuacao />
     </UpdateContainer>
   )
 }
