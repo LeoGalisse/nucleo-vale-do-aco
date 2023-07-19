f82690c6ab3788122163c405f65d8fbd44ce68ff
LeoGalisse <leonardogalisse@gmail.com>
2023-07-18 21:46:56 -0300

    feat: Convert StaticProps to ServerProps

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,30 +1,14 @@
 import axios from 'axios'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 
 export { default } from './Home'
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const response = await axios.get(`${process.env.API_URL}/numbers`)
 
-  if (response) {
-    return {
-      props: {
-        numbers: response.data.data[0],
-      },
-      revalidate: 60 * 60 * 2, // 2 hours
-    }
-  } else {
-    return {
-      props: {
-        numbers: {
-          id: '2ce30d2f-235c-4615-890a-962d976fa99a',
-          number_of_cities: 3,
-          number_of_ejs: 17,
-          number_of_employees: 161,
-          number_of_institutes: 5,
-        },
-      },
-      revalidate: 60 * 5, // 5 minutes
-    }
+  return {
+    props: {
+      numbers: response.data.data[0],
+    },
   }
 }
--- src/pages/quemsomos/index.page.tsx
+++ src/pages/quemsomos/index.page.tsx
@@ -8,7 +8,7 @@ import minasGerais from '../../assets/minas-gerais.svg'
 import { AboutSection } from './components/AboutSection'
 import { FrentesDeAtuacaoSection } from './components/FrentesDeAtuacaoSection'
 import { NextSeo } from 'next-seo'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import axios from 'axios'
 
 export default function QuemSomos(props: any) {
@@ -37,7 +37,7 @@ export default function QuemSomos(props: any) {
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const responseAbout = await axios.get(`${process.env.API_URL}/about`)
   const res = responseAbout.data.about_section.split('/')
 
@@ -45,35 +45,11 @@ export const getStaticProps: GetStaticProps = async () => {
     `${process.env.API_URL}/frentes-de-atuacao`,
   )
 
-  if (res && responseFrentesDeAtuacao.data) {
-    return {
-      props: {
-        about: res,
-        frentesDeAtuacao: responseFrentesDeAtuacao.data,
-      },
-      revalidate: 60 * 60 * 2, // 2 hours
-    }
-  } else {
-    return {
-      props: {
-        about: [
-          'Fazemos parte do Movimento Empresa Júnior, sendo o núcleo responsável por representar as empresas Juniores do Vale do Aço e Médio Piracicaba. Nossa missão é Transformar o Vale do Aço através do desenvolvimento e união das EJs do NúVA e crescimento da rede, impactando e transformando empresários juniores em líderes capazes de atingir seus objetivos e revolucionar sua realidade.',
-          '  O núcleo é formado atualmente por 17 empresas juniores da região dos mais variados cursos de graduação. Cada empresa é formada por estudantes de ensino superior que trabalham voluntariamente realizando projetos para cliente reais.',
-          '  Nosso time é formado por quatro diretorias: presidência do conselho, presidência executiva, diretoria de expansão e diretoria de desenvolvimento. Cada uma delas também é composta por empresários juniores que assumiram o desafio de prestar suporte para todas as empresas da região. Estas pessoas têm como objetivo alavancar os resultados e a experiência dessas pessoas para que se desenvolvam cada vez mais.',
-        ],
-        frentesDeAtuacao: {
-          FrentesAtuacaoSection: [
-            {
-              id: 'e257b8f1-ff61-4dcb-9901-5abbd29d450f',
-              title: 'Presidencia do Conselho',
-              description: [Array],
-              quemSomosPageId: 'b5fc77eb-c717-4fcf-ab1d-65bae5288cc1',
-            },
-          ],
-          id: 'b5fc77eb-c717-4fcf-ab1d-65bae5288cc1',
-        },
-      },
-      revalidate: 60 * 5, // 5 minutes
-    }
+  return {
+    props: {
+      about: res,
+      frentesDeAtuacao: responseFrentesDeAtuacao.data,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
